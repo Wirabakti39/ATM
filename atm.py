33b72da6f58092f94516798cfddb40bf6e8b0d57
Dimas <94775622+Wirabakti39@users.noreply.github.com>
2022-03-18 10:33:25 +0800

    Update atm.py
    Penambahan fitur terakhir, yaitu nabung uang. Dan perbaikan algoritma

--- atm.py
+++ atm.py
@@ -27,7 +27,7 @@ kartu_BCA = {
 'jumlah' : 300000
 }
 kartu_mandiri = {
-'nomor' : '040307',
+'nomor' : '040307',		#ubah nomor atau password kartu sesuka kalian
 'password' : 'yunita',
 'jumlah' : 250000
 }
@@ -38,44 +38,50 @@ kartu_lainnya = {
 }
 
 print("\twelcome to Dimaz's Bank.")
-print('\nLanjut dengan menggunakan kartu?\n1. Kartu BCA\n2. Kartu mandiri\n3. Kartu lainnya -')
-kartu = input('pilihan kartu anda : ')
-if kartu == '1' or kartu == 'BCA' :
-	kartu = kartu_BCA
-	fitur.cekPw(kartu, kartu['nomor'], kartu['password'])	
-	while fitur.salah :
-		a = input("ketik '1' untuk mencoba lagi : ")
-		if a != '1' : break
-		fitur.cekPw(kartu, kartu['nomor'], kartu['password'])		
-	if fitur.salah == False :
-		print("berikut adalah beberapa pelayanan mesin ini")
-		print("1. Transfer uang\n2. Tarik uang tunai\n3. Taruh/tabung uang")
-		fitur.opsiClient(kartu, kartu_mandiri, kartu_lainnya)
-		
-elif kartu == '2' or kartu == 'mandiri' :
-	kartu = kartu_mandiri
-	fitur.cekPw(kartu, kartu['nomor'], kartu['password'])	
-	while fitur.salah :
-		a = input("ketik '1' untuk mencoba lagi : ")
-		if a != '1' : break
-		fitur.cekPw(kartu, kartu['nomor'], kartu['password'])		
-	if fitur.salah == False :
-		print("berikut adalah beberapa pelayanan mesin ini")
-		print("1. Transfer uang\n2. Tarik uang tunai\n3. Taruh/tabung uang")
-		fitur.opsiClient(kartu, kartu_BCA, kartu_lainnya)
+while True :
+	print('\nLanjut dengan menggunakan kartu?\n1. Kartu BCA\n2. Kartu mandiri\n3. Kartu lainnya -')
+	kartu = input('pilihan kartu anda : ')
+	if kartu == '1' or kartu == 'BCA' :
+		kartu = kartu_BCA
+		fitur.cekPw(kartu, kartu['nomor'], kartu['password'])	
+		while fitur.salah :
+			a = input("ketik '0' untuk mencoba lagi : ")
+			if a != '0' : break
+			fitur.cekPw(kartu, kartu['nomor'], kartu['password'])		
+		if fitur.salah == False :
+			print("berikut adalah beberapa pelayanan mesin ini")
+			print("1. Transfer uang\n2. Tarik uang tunai\n3. Taruh/tabung uang")
+			fitur.opsiClient(kartu, kartu_mandiri, kartu_lainnya)
+			break
+		break	
+	elif kartu == '2' or kartu == 'mandiri' :
+		kartu = kartu_mandiri
+		fitur.cekPw(kartu, kartu['nomor'], kartu['password'])	
+		while fitur.salah :
+			a = input("ketik '0' untuk mencoba lagi : ")
+			if a != '0' : break
+			fitur.cekPw(kartu, kartu['nomor'], kartu['password'])		
+		if fitur.salah == False :
+			print("berikut adalah beberapa pelayanan mesin ini")
+			print("1. Transfer uang\n2. Tarik uang tunai\n3. Taruh/tabung uang")
+			fitur.opsiClient(kartu, kartu_BCA, kartu_lainnya)
+			break
+		break	
+	elif kartu == '3' or kartu == 'lainnya' :
+		kartu = kartu_lainnya
+		fitur.cekPw(kartu, kartu['nomor'], kartu['password'])	
+		while fitur.salah :
+			a = input("ketik '0' untuk mencoba lagi : ")
+			if a != '0' : break
+			fitur.cekPw(kartu, kartu['nomor'], kartu['password'])		
+		if fitur.salah == False :
+			print("berikut adalah beberapa pelayanan mesin ini")
+			print("1. Transfer uang\n2. Tarik uang tunai\n3. Taruh/tabung uang")
+			fitur.opsiClient(kartu, kartu_BCA, kartu_mandiri)
+			break
+		break	
+	else :
+		print("\nMaaf, permintaan anda tidak dapat kami proses.\nMohon coba lagi.")
+		a = input("\n\tketik '0' untuk mencoba lagi : ")
+		if a != '0' : break
 
-elif kartu == '3' or kartu == 'lainnya' :
-	kartu = kartu_lainnya
-	fitur.cekPw(kartu, kartu['nomor'], kartu['password'])	
-	while fitur.salah :
-		a = input("ketik '1' untuk mencoba lagi : ")
-		if a != '1' : break
-		fitur.cekPw(kartu, kartu['nomor'], kartu['password'])		
-	if fitur.salah == False :
-		print("berikut adalah beberapa pelayanan mesin ini")
-		print("1. Transfer uang\n2. Tarik uang tunai\n3. Taruh/tabung uang")
-		fitur.opsiClient(kartu, kartu_BCA, kartu_mandiri)
-
-else :
-	print("\nMaaf, permintaan anda tidak dapat kami proses.\n\tMohon coba lagi.")
-	
