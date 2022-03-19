8a868a23a63d6f9ebca10adfd8c2b64ed746a78a
Dimas <94775622+Wirabakti39@users.noreply.github.com>
2022-03-20 00:02:16 +0800

    Final update fitur_atm.py
    Debugging, repair, etc.

--- fitur_atm.py
+++ fitur_atm.py
@@ -1,7 +1,9 @@
+#4. Algoritma, function/method di balik layar.
+
 #fungsi ngecek nomor + password kartu pilihan
 def cekPw(card, no, pw) :
-	n = input(f"\n\tmasukan nomor kartu anda : ")
-	p = input(f"\tmasukan password dari kartu {n} : ")
+	n = input(f'\n\tmasukan nomor dari {card["nama"]} anda : ')
+	p = input(f'\tmasukan password dari {card["nama"]} anda : ')
 	
 	global salah
 	if n == no and p == pw :
@@ -9,41 +11,56 @@ def cekPw(card, no, pw) :
 		print(f"\nRegistrasi sukses.\nUang dalam kartu {no} berjumlah Rp. {card['jumlah']}\n")
 	else :
 		salah = True
-		print('\n\tnomor atau password kartu anda invalid.\n')
+		print('\nNomor / password tidak cocok dengan ', card["nama"], ' anda.\n')
 
 #fungsi dari fitur transfer uang
 def transfer_uang(card, card1, card2) :
 	while True :
-		transfer = int(input("\n\tmasukan jumlah yang akan ditransfer : Rp. "))
-		tujuan = input(f"\tuang Rp. {transfer} akan di transfer ke?\n\tmasukan no. kartu yang dituju : ")
-		card["jumlah"] -= transfer
-		if tujuan == card1["nomor"] :
-			card1["jumlah"] += transfer
-			print(f'\nTransaksi ke nomor kartu {card1["nomor"]} sukses.\nRp. {transfer} telah dikurangi dari saldo anda')
-			break
-			return card, card1
-		elif tujuan == card2["nomor"] :
-			card2["jumlah"] += transfer
-			print(f'\nTransaksi ke nomor kartu {card2["nomor"]} sukses.\nRp. {transfer} telah dikurangi dari saldo anda')
-			break
-			return card, card2
-		elif tujuan == card["nomor"] :
-			print("\nMaaf, anda tidak bisa memasukan nomor kartu anda sendiri.")
-			z = input("\nKetik 0 untuk coba lagi : ")
-			if z != "0" : break
-		else :
-			print("\nTransaksi gagal,\nmesin ini tidak menyediakan layanan untuk nomor ", tujuan)
-			z = input("\nKetik 0 untuk coba lagi : ")
-			if z != "0" : break												
+		try :
+			transfer = int(input("\n\tmasukan jumlah yang akan ditransfer : Rp. "))
+			tujuan = input(f"\tuang Rp. {transfer} akan di transfer ke?\n\tmasukan nomor kartu yang dituju : ")
+			if transfer > card["jumlah"] :
+				print(f'\nTransfer gagal, isi saldo {card["nama"]} anda tidak mencukupi')
+				z = input("\n\tketik 0 untuk coba lagi : ")															
+				if z != "0" : break
+			else :
+				card["jumlah"] -= transfer
+				if tujuan == card1["nomor"] :
+					card1["jumlah"] += transfer
+					print(f'\nTransaksi ke nomor kartu {card1["nomor"]} sukses.\nRp. {transfer} telah dikurangi dari saldo anda')
+					break
+					return card, card1
+				elif tujuan == card2["nomor"] :
+					card2["jumlah"] += transfer
+					print(f'\nTransaksi ke nomor kartu {card2["nomor"]} sukses.\nRp. {transfer} telah dikurangi dari saldo anda')
+					break
+					return card, card2
+				elif tujuan == card["nomor"] :
+					print("\nMaaf, anda tidak bisa memasukan nomor kartu anda sendiri.")
+					z = input("\n\tKetik 0 untuk coba lagi : ")
+					if z != "0" : break
+				else :
+					print("\nTransaksi gagal,\nmesin ini tidak menyediakan layanan untuk nomor ", tujuan)
+					z = input("\n\tKetik 0 untuk coba lagi : ")
+					if z != "0" : break	
+		except ValueError :
+			z = input("\nYang anda masukan bukan angka, coba lagi.\n\n\tketik 0 untuk coba lagi : ")
+			if z != "0" : break							
 
 #fungsi dari fitur tarik tunai
 def tarik_tunai(card) :
 	while True :
 		try :
 			jumlah = int(input("\n\tmasukan jumlah uang yang akan ditarik : Rp "))
-			card["jumlah"] -= jumlah
-			print("\nPenarikan uang sejumlah ", jumlah, " berhasil.")
-			break																																																																																																																																																																																																																		
+			if jumlah > card["jumlah"] :
+				print(f'\nPenarikan gagal, isi saldo {card["nama"]} anda tidak mencukupi')
+				z = input("\n\tketik 0 untuk coba lagi : ")															
+				if z != "0" : break	
+			else :
+				card["jumlah"] -= jumlah
+				print("\nPenarikan uang sejumlah Rp. ", jumlah, " berhasil.")
+				break		
+				return card																																																																																																																																																																																																																
 		except ValueError :
 			z = input("\nYang anda masukan bukan angka, coba lagi.\n\n\tketik 0 untuk coba lagi : ")
 			if z != "0" : break	
@@ -57,10 +74,11 @@ def tabung_uang(card) :
 			if uang >= nabung :
 				card["jumlah"] += nabung																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 				uang -= nabung																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
-				print(f'\nProses sukses, jumlah uang yg anda pegang sekarang Rp. {uang}\nRp. {nabung} berhasil ditambahkan ke kartu anda ({card["nomor"]})')	
+				print(f'\nBerhasil, jumlah uang yg anda pegang sekarang Rp. {uang}\nRp. {nabung} berhasil ditambahkan ke {card["nama"]} ({card["nomor"]}) anda.')	
 				break
+				return card
 			else :					
-				print("Proses menabung gagal.\nJumlah uang anda kurang dari jumlah yang ingin anda tabung")										
+				print("Proses menabung gagal.\nJumlah uang yang anda pegang ( < ) jumlah yang akan ditabung")										
 				z = input("\n\tketik 0 untuk coba lagi : ")															
 				if z != "0" : break																			
 		except ValueError :																																	
@@ -69,22 +87,23 @@ def tabung_uang(card) :
 			if z != "0" : break
 
 #fungsi untuk memilih fitur yang akan digunakan																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																										
-def opsiClient(jenis_kartu_anda, jenis_1, jenis_2) :
+def opsiClient(kartu_anda, kartu_1, kartu_2) :
 	while True :
 		opsi = input("Silahkan pilih keperluan anda : ")
 		if opsi == "1" :
-			transfer_uang(jenis_kartu_anda, jenis_1, jenis_2)
-			print(f'\nSisa saldo kartu anda ({jenis_kartu_anda["nomor"]}) : Rp. {jenis_kartu_anda["jumlah"]}')
+			transfer_uang(kartu_anda, kartu_1, kartu_2)
+			print(f'\nSisa saldo {kartu_anda["nama"]} ({kartu_anda["nomor"]}) anda : Rp. {kartu_anda["jumlah"]}')
 			break
 		elif opsi == "2" :
-			tarik_tunai(jenis_kartu_anda)
-			print(f'\nSisa saldo kartu anda ({jenis_kartu_anda["nomor"]}) : Rp. {jenis_kartu_anda["jumlah"]}')
+			tarik_tunai(kartu_anda)
+			print(f'\nSisa saldo {kartu_anda["nama"]} ({kartu_anda["nomor"]}) anda : Rp. {kartu_anda["jumlah"]}')
 			break
 		elif opsi == "3" :
-			tabung_uang(jenis_kartu_anda)
-			print(f'\nSisa saldo kartu anda ({jenis_kartu_anda["nomor"]}) : Rp. {jenis_kartu_anda["jumlah"]}')
+			tabung_uang(kartu_anda)
+			print(f'\nSisa saldo {kartu_anda["nama"]} ({kartu_anda["nomor"]}) anda : Rp. {kartu_anda["jumlah"]}')
 			break
 		else :
 			print("\nMaaf, mesin ini tidak menyediakan permintaan anda.")
 			z = input("\n\tketik 0 untuk coba lagi : ")
+			print("\n")
 			if z != "0" : break
